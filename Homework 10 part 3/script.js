bef1cb1d9510fce4ef043f9bf7f430341544ee24
marting <47759340+martingulabovski@users.noreply.github.com>
2021-12-25 09:16:00 +0100

    Homework 10 part 3

---
+++ Homework 10 part 3/script.js
@@ -0,0 +1,29 @@
+let userName = "Martin";
+let pinNumber = 3241;
+let logIn = prompt("Enter your pin number Mr." + userName);
+if (logIn != pinNumber) {
+  alert("Incorrect pin! Refresh page to try again.");
+}
+
+function withdrawlMoney(amount) {
+  let balance = 2000;
+  if (amount > balance) {
+    alert("Not enough funds on your account!");
+    return false;
+  } else if ((balance = balance - amount)) {
+    alert(
+      "Successful withdrawal of: US $" +
+        amount +
+        "\nAvailable Balance is: US $" +
+        balance
+    );
+  } else if (isNaN(amount)) {
+    alert("That is not an amount.");
+  }
+}
+
+function enterAmount() {
+  let amount = document.getElementById("amount").value;
+  withdrawlMoney(amount);
+}
+document.getElementById("enter").addEventListener("click", enterAmount);
