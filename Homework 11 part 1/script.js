c705a7fad8326b1bc487d6b57ddf816b39dda27e
marting <47759340+martingulabovski@users.noreply.github.com>
2021-12-27 03:37:41 +0100

    Homework 11 part 1

---
+++ Homework 11 part 1/script.js
@@ -0,0 +1,20 @@
+let prompts = [
+  prompt("Enter your name "),
+  prompt("What is your mood today?"),
+  prompt("What kind of activity did you do today?"),
+];
+
+
+alert(
+  "My name is " +
+    prompts[0] +
+    ". " +
+    prompts[0] +
+    " is a nice person! " +
+    " Today their mood is " +
+    prompts[1] +
+    ". " +
+    "They were " +
+    prompts[2] +
+    " all day."
+);
