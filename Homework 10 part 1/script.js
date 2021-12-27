61496e495668f4ce60d53f8df46e7d3c4de0a505
marting <47759340+martingulabovski@users.noreply.github.com>
2021-12-27 05:58:15 +0100

    Homework 10 part 1

---
+++ Homework 10 part 1/script.js
@@ -0,0 +1,14 @@
+console.log(typeof 42);
+
+function test(a, b, c, d, e) {
+  return typeof a;
+  return typeof b;
+  return typeof c;
+  return typeof d;
+  return typeof e;
+}
+
+console.log(test("Random text"));
+console.log(test(true));
+console.log(test(undefined));
+console.log(test(new Object()));
