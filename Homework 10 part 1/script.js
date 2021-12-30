a008fe626eb771b3b28a1b8f788f7884582493bb
marting <47759340+martingulabovski@users.noreply.github.com>
2021-12-30 20:12:12 +0100

    Update script.js

--- Homework 10 part 1/script.js
+++ Homework 10 part 1/script.js
@@ -1,9 +1,5 @@
-function test(a, b, c, d, e) {
+function test(a) {
   return typeof a;
-  return typeof b;
-  return typeof c;
-  return typeof d;
-  return typeof e;
 }
 console.log(test(401));
 console.log(test("Random text"));
