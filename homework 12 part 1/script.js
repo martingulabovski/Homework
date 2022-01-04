e6d19fb0020a154f78136317f32ba7565c2f36ea
marting <47759340+martingulabovski@users.noreply.github.com>
2022-01-04 20:06:21 +0100

    Homework 12 part 1

---
+++ homework 12 part 1/script.js
@@ -0,0 +1,7 @@
+let array = ["Welcome"," there","students","of","SEDC","!"];
+
+function message() {
+  return `${array[0]}${array[1]} ${array[2]} ${array[3]} ${array[4]}${array[5]}`;
+}
+
+alert(message());
